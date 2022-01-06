d0f4da68edf26837ac6aad7fbd8afe2dff7fca6f
ThaddG <thaddious.gorges@gmail.com>
2022-01-06 02:21:09 -0500

    added react native paper

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+import * as React from 'react';
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import App from './App';
+
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Main);
