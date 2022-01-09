56bc89f98f28815f4910b6ce2d320153f8900e25
ThaddG <thaddious.gorges@gmail.com>
2022-01-08 20:06:55 -0500

    skeleton for the user reducer implemented

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Button} from 'react-native-paper';
+import { useAppDispatch, useAppSelector } from './src/redux/hooks';
+
+import { signup } from './src/redux/user/userSlice';
 
 export default function App() {
+  const user = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button onPress={() => dispatch(signup())}>
+        signup
+      </Button>
+      <Text>{user.email}</Text>
     </View>
   );
 }
